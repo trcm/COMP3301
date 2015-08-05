3554666dad83318e753c6c0b6a589e37f4cd61e6
tom <tom@foo.localdomain>
2015-08-05 13:08:06 +1000

    working on libevent

--- ass1/mirrord.c
+++ ass1/mirrord.c
@@ -1,17 +1,24 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <arpa/inet.h>
 
+#include <event.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <err.h>
 #include <string.h>
 
 
-__dead	 void usage(void);
+int 	 start_mirror(FILE *, char*, char*);	
+void 	 ack_con(int, short, void *);
+int  	 print_to_log(FILE*, char*);
+__dead void	usage(void);
 
 __dead void
 usage(void)
@@ -22,13 +29,74 @@ usage(void)
 }
 
 int
+print_to_log(FILE* logfile, char* message)
+{
+    if (logfile == NULL)
+    {
+	printf("%s\n", message);
+    } else {
+	fputs(message, logfile);
+    }
+
+    return 0;
+} 
+
+void
+ack_con(int socket, short revents, void *logfile)
+{
+    int fd;
+    struct sockaddr_storage ss;
+    socklen_t socklen = sizeof(ss);
+    /* printf("accepted connection from \n"); */
+    fd = accept(socket, (struct sockaddr *)&ss, &socklen);
+    print_to_log(logfile, "logged yo");
+    send(fd, "sup", 3, 0);
+
+    close(fd);
+}
+
+int
+start_mirror(FILE* logfile, char* hostname, char* port)
+{
+    struct addrinfo hints, *res;
+    struct event event;
+    int error;
+    int s;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    error = getaddrinfo(hostname, port, &hints, &res);
+    if (error)
+	errx(1, "%s", gai_strerror(error));
+
+    s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    bind(s, res->ai_addr, res->ai_addrlen);
+
+    listen(s, 1);
+
+    event_init();
+
+    event_set(&event, s, EV_READ | EV_PERSIST, ack_con, logfile);
+    event_add(&event, NULL);
+
+    event_dispatch();
+    
+    
+    freeaddrinfo(res);
+    return 0;
+}
+
+int
 main (int argc, char *argv[])
 {
     /* IP version and int for getopt */
-    int ch, ip, port, portFlag = 0;
+    int ch, ip, daemon = 1, portFlag = 0;
     /* char* log, address, dirname, port; */
-    char* log, *address, *dirname ;
-
+    char* log, *address = NULL, *dirname, *port =NULL;
+    FILE* logfile = NULL;
     // check for the minimum number of command line arguments
     if (argc < 2 || argc > 9)
 	usage();
@@ -47,10 +115,13 @@ main (int argc, char *argv[])
 	    break;
 	case 'd':
 	    printf("dont daemonize\n");
+	    daemon = 0;
 	    break;
 	case 'a':
 	    printf("log: %s\n", optarg);
 	    log = optarg;
+	    logfile = fopen(log, "a");
+	    fputs("mirrord started.\n", logfile);
 	    break;
 	case 'l':
 	    printf("address: %s\n", optarg);
@@ -58,28 +129,51 @@ main (int argc, char *argv[])
 	    break;
 	case 'p':
 	    printf("port: %s\n", optarg);
-	    port = atoi(optarg);
-	    portFlag = 1;
+	    port = optarg;
+	    portFlag = 0;
 	    break;
 	default:
 	    usage();
 	}
     }
 
-    if (!portFlag)
-    {
-	struct servent * s = getservbyname("http", "tcp");
-	printf("no port supplied, finding port: ");
-	if (s) {
-	    printf("%d\n", s->s_port);
-	    port = s->s_port;
+    /* if (!portFlag) */
+    /* { */
+    /* 	struct servent * s = getservbyname("http", "tcp"); */
+    /* 	printf("no port supplied, finding port: "); */
+    /* 	if (s) { */
+    /* 	    printf("%d\n", s->s_port); */
+    /* 	    port = s->s_port; */
+    /* 	} else { */
+    /* 	    err(1, "Could not find port\n"); */
+    /* 	} */
+    /* } */
+
+    if (daemon) {
+	/* daemonize the process */
+	/* int status, i; */
+	pid_t pid, sid;
+	if ((pid = fork()) == 0) {
+	    
+	    umask(0);
+	    
+	    sid = setsid();
+	    close(STDOUT_FILENO);
+	    close(STDIN_FILENO);
+	    close(STDERR_FILENO);
+	    start_mirror(logfile, address, port);
+	    
 	} else {
-	    err(1, "Could not find/use port");
+	    exit(0);
 	    
 	}
+    } else {
+	printf("don't daemonize");
+	/* don't daemonize */
+	start_mirror(NULL, address, port);
     }
 
-
+    
     dirname = argv[argc - 1];
     printf("dirname: %s\n", dirname);
     return 0;
--- prac2/prac2-event/echod.c
+++ prac2/prac2-event/echod.c
@@ -16,9 +16,9 @@
 __dead void usage(void);
 
 struct conn {
-	struct event		rd_ev;
-	struct event		wr_ev;
-	struct evbuffer		*buf;
+    struct event		rd_ev;
+    struct event		wr_ev;
+    struct evbuffer		*buf;
 };
 
 void echo_accept(int, short, void *);
@@ -29,71 +29,83 @@ void echo_close(struct conn *);
 void
 echo_accept(int sock, short revents, void *null)
 {
-	struct sockaddr_storage ss;
-	socklen_t socklen = sizeof(ss);
-	struct conn *c;
-	int fd;
-	int on = 1;
+    struct sockaddr_storage ss;
+    socklen_t socklen = sizeof(ss);
+    struct conn *c;
+    int fd;
+    int on = 1;
 
-	fd = accept(sock, (struct sockaddr *)&ss, &socklen);
-	if (fd == -1) {
-		switch (errno) {
-		case EINTR:
-		case EWOULDBLOCK:
-		case ECONNABORTED:
-			/* oh well, wait for another one */
-			return;
-		default:
-			err(1, "accept");
-		}
+    fd = accept(sock, (struct sockaddr *)&ss, &socklen);
+    if (fd == -1) {
+	switch (errno) {
+	case EINTR:
+	case EWOULDBLOCK:
+	case ECONNABORTED:
+	    /* oh well, wait for another one */
+	    return;
+	default:
+	    err(1, "accept");
 	}
+    }
 
-	if (ioctl(fd, FIONBIO, &on) == -1)
-		err(1, "conn ioctl(FIONBIO)");
+    if (ioctl(fd, FIONBIO, &on) == -1)
+	err(1, "conn ioctl(FIONBIO)");
 
-	c = malloc(sizeof(*c));
-	if (c == NULL) {
-		warn("conn alloc");
-		close(fd);
-		return;
-	}
+    c = malloc(sizeof(*c));
+    if (c == NULL) {
+	warn("conn alloc");
+	close(fd);
+	return;
+    }
 
-	c->buf = evbuffer_new();
-	if (c->buf == NULL) {
-		warn("conn buf alloc");
-		free(c);
-		close(fd);
-		return;
-	}
+    c->buf = evbuffer_new();
+    if (c->buf == NULL) {
+	warn("conn buf alloc");
+	free(c);
+	close(fd);
+	return;
+    }
 
-	printf("fd %d: accepted\n", fd);
+    printf("fd %d: accepted\n", fd);
 
-	event_set(&c->rd_ev, fd, EV_READ | EV_PERSIST, echo_read, c);
-	event_set(&c->wr_ev, fd, EV_WRITE, echo_write, c);
-	event_add(&c->rd_ev, NULL);
-	event_add(&c->wr_ev, NULL);
+    event_set(&c->rd_ev, fd, EV_READ | EV_PERSIST, echo_read, c);
+    event_set(&c->wr_ev, fd, EV_WRITE, echo_write, c);
+    event_add(&c->rd_ev, NULL);
+    event_add(&c->wr_ev, NULL);
 }
-
 
 void
 echo_read(int fd, short revents, void *conn)
 {
+    int i;
 // create buffer
 /* struct evbuffer * eBuff = evbuffer_new(); */
-struct conn * c = (struct conn *) conn;
-/* c->buf = evbuffer_new(); */
-/* printf("\n\n%s", (char *) c->buf); */
+    struct conn * c = (struct conn *) conn;
 // recieve and echo the message to the user
-evbuffer_read(c->buf, fd, 1024);
-fprintf(stdout, "fd %d: %s\n", fd, (char *)c->buf);
+    char ** data;
+    data =  malloc(sizeof(char*) * 1024);
+    for (i = 0; i < 1024; i++) {
+	data[i] = malloc(sizeof(char) * 1024);
+    }
+    int dLen = 0;
+    do {
+	evbuffer_read(c->buf, fd, 1024);
+	printf("%s %d\n", c->buf->buffer, dLen);
+	if (strcmp(c->buf->buffer, data[dLen]) == 0) {
+	    data[dLen] = c->buf->buffer;
+	    evbuffer_drain(c->buf, strlen(data[dLen]));
+	    dLen++;
+	}
+    } while (strcmp(c->buf->buffer, "done\n") != 0);
+
+/* evbuffer_read(c->buf, fd, 1024); */
+    fprintf(stdout, "fd %d: %s\n", fd, (char *)c->buf->buffer);
 /* fflush(stdout); */
-write();
-evbuffer_write(c->buf, fd);
+    write(fd, "Message recieved\n", 18);
+    evbuffer_write(c->buf, fd);
 /* evbuffer_write(c->evbuffer, fd); */
 // free the buffer
-/* evbuffer_free(eBuff); */
-/* evbuffer_free(c->buf); */
-echo_close(conn);
+    echo_close(conn);
 }
 
 
@@ -101,65 +113,65 @@ echo_close(conn);
 void
 echo_write(int fd, short revents, void *conn)
 {
-printf("fd %d: wrote echo\n", fd);
+    printf("fd %d: wrote echo\n", fd);
 }
 
 
 void
 echo_close(struct conn *c)
 {
-	printf("fd %d: closing\n", EVENT_FD(&c->rd_ev));
+    printf("fd %d: closing\n", EVENT_FD(&c->rd_ev));
 
-	evbuffer_free(c->buf);
-	event_del(&c->wr_ev);
-	event_del(&c->rd_ev);
-	close(EVENT_FD(&c->rd_ev));
-	free(c);
+    evbuffer_free(c->buf);
+    event_del(&c->wr_ev);
+    event_del(&c->rd_ev);
+    close(EVENT_FD(&c->rd_ev));
+    free(c);
 }
 
 __dead void
 usage(void)
 {
-	extern char *__progname;
-	fprintf(stderr, "usage: %s sock\n", __progname);
-	exit(1);
+    extern char *__progname;
+    fprintf(stderr, "usage: %s sock\n", __progname);
+    exit(1);
 }
 
 int
 main(int argc, char *argv[])
 {
-	struct sockaddr_un sun;
-	struct event event;
-	int sock;
-	int on = 1;
+    struct sockaddr_un sun;
+    struct event event;
+    int sock;
+    int on = 1;
 
-	if (argc != 2)
-		usage();
+    if (argc != 2)
+	usage();
 
-	sun.sun_family = AF_UNIX;
-	if (strlcpy(sun.sun_path, argv[1],
-	    sizeof(sun.sun_path)) >= sizeof(sun.sun_path))
-		errx(1, "socket path is too long");
+    sun.sun_family = AF_UNIX;
+    if (strlcpy(sun.sun_path, argv[1],
+		sizeof(sun.sun_path)) >= sizeof(sun.sun_path))
+	errx(1, "socket path is too long");
 
-	sock = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (sock == -1)
-		err(1, "socket");
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock == -1)
+	err(1, "socket");
 
-	if (bind(sock, (struct sockaddr *)&sun, sizeof(sun)) == -1)
-		err(1, "bind");
+    if (bind(sock, (struct sockaddr *)&sun, sizeof(sun)) == -1)
+	err(1, "bind");
 
-	if (ioctl(sock, FIONBIO, &on) == -1)
-		err(1, "listener ioctl(FIONBIO)");
+    if (ioctl(sock, FIONBIO, &on) == -1)
+	err(1, "listener ioctl(FIONBIO)");
 
-	if (listen(sock, 5) == -1)
-		err(1, "listen");
+    if (listen(sock, 5) == -1)
+	err(1, "listen");
 
-	event_init();
+    event_init();
 
-	event_set(&event, sock, EV_READ | EV_PERSIST, echo_accept, NULL);
-	event_add(&event, NULL);
+    event_set(&event, sock, EV_READ | EV_PERSIST, echo_accept, NULL);
+    event_add(&event, NULL);
 
-	event_dispatch();
+    event_dispatch();
 
-	return (0);
+    return (0);
 }
