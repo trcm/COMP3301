37f3f8231bed4c47008ed52d571aec0ed6f1319b
tom <tom@foo.localdomain>
2015-08-07 20:46:37 +1000

    finished logging for 200 and 404

--- ass1/mirrord.c
+++ ass1/mirrord.c
@@ -4,6 +4,7 @@
 #include <sys/ioctl.h>
 #include <sys/wait.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 
 #include <event.h>
 
@@ -21,13 +22,16 @@
 #include "mirrord.h"
 
 #define MAX_REQUESTS 1024
+#define CHUNK 1024
 
 /* global http_parser_settings for all connections */
 static http_parser_settings settings;
+/* static char* logfile; */
 
 request requests[MAX_REQUESTS];
 int requestNum;
-
+int logFlag;
+FILE * logptr;
 
 __dead void
 usage(void)
@@ -38,14 +42,14 @@ usage(void)
 }
 
 int
-print_to_log(FILE *logfile, char *message)
+print_to_log(char *message)
 {
-	if (logfile == NULL) {
+	if (logptr == NULL && logFlag) {
 		fprintf(stdout, message);
 		fflush(stdout);
-	} else {
-		fprintf(logfile, "%s\n", message);
-		fflush(logfile);
+	} else if (logptr) {
+		fprintf(logptr, "%s", message);
+		fflush(logptr);
 	}
 
 	return 0;
@@ -59,15 +63,22 @@ ack_con(int sock, short revents, void *logfile)
 	socklen_t 	socklen = sizeof(ss);
 	
 	fd = accept(sock, (struct sockaddr *) & ss, &socklen);
-	printf("%d\n", fd);
 	// init http_parser
 	http_parser * hp = malloc(sizeof(http_parser));
 	http_parser_init(hp, HTTP_REQUEST);
 
-
+	
+	struct sockaddr_in *s = (struct sockaddr_in *) &ss;
+	char *address = inet_ntoa(s->sin_addr);
+	
+	strncat(requests[requestNum].remote_addr, "\0", 1);
+	strncat(requests[requestNum].remote_addr, address, strlen(address));
+	
+	
 	hp->data = &fd;
 	/* printf("%d\n", hp->data); */
 	char * req = malloc(sizeof(char) * 1024);
+	requests[requestNum].headerNum = 0;
 
 	/* do { */
 	do{
@@ -118,43 +129,123 @@ start_mirror(FILE *logfile, char *hostname, char *port)
 int
 on_complete(http_parser *parser)
 {
-	printf("Message recieved %d\n", parser->method);
+
 	switch (parser->method)
 	{
 	case 1:
-		
-		print_to_log(NULL, "remote arrd [rfc822date] \"GET request_url\" response_code data_bytes\n");
+		strncpy(requests[requestNum].method, "GET", 3);
 		break;
 	case 2:
-		
-		print_to_log(NULL, "remote arrd [rfc822date] \"HEAD request_url\" response_code data_bytes\n");
+		strncpy(requests[requestNum].method, "HEAD", 4);
+		break;
+	default:
+		/* strncpy(requests[requestNum].method, "HEAD", 4); */
+		/* method not supported */
 		break;
 	}
 	/* } while (sendLen > 0); */
-
-	int * fd = (int *)parser->data;
 	int f = retrieve_file(requests[requestNum].url);
+	int * fd = (int *)parser->data;
 	if (f > 0) {
 		struct stat st;
 		fstat(f, &st);
 		char *res = malloc(sizeof(char)*1024);
+
+		/* create time buffers for headers and log */
+		time_t curr;
+		struct tm *modtime;
+		struct tm *currtime;
+		modtime = gmtime(&st.st_mtime);
+		time(&curr);
+		currtime = gmtime(&curr);
+		char buff[30];
+		char cbuff[30];
+		strftime(buff, 30, "%a, %d %b %Y %T GMT", modtime);
+		strftime(cbuff, 30, "%a, %d %b %Y %T GMT", currtime);
+
 		/* create response header */
 		sprintf(res,  
 			"HTTP/1.1 200 OK\n"	    \
-			"Content-Type: text/html\n" \
+			"Date: %s\n" \
+			"Last-modified: %s\n" \
 			"Content-Length: %jd\n" \
 			"Connection: close\n" \
 			"Server: mirrord/s4333060\n" \
-			"\r\n", st.st_size);
-			
+			"\r\n", cbuff, buff, st.st_size);
 		int sendLen = 0;
-		/* do { */
+		/* send response header */
 		sendLen = send(*fd, res, strlen(res), MSG_NOSIGNAL);
-		printf("sending file");
-		char fData[1024];
-		read(f, fData, st.st_size);
-		send(*fd, fData, st.st_size, MSG_NOSIGNAL);
+
+		/* start reading file  */
+		void *fData = malloc(sizeof(char)*CHUNK);
+		size_t nBytes;
+		do {
+			/* read chunk from file */
+			nBytes = read(f, fData, CHUNK);
+			/* send chunk */
+			send(*fd, fData, nBytes, MSG_NOSIGNAL);
+		} while (nBytes > 0);
+		
+		/* int i, hNum = requests[requestNum].headerNum; */
+		/* /\* get host name for log *\/ */
+		/* char fullHost[30]; */
+		/* for (i = 0; i < hNum; i++) */
+		/* { */
+		/* 	if (strcmp(requests[requestNum].headerFields[i], "Host") == 0) */
+		/* 	{ */
+		/* 		strcpy(fullHost, requests[requestNum].headerValues[i]); */
+		/* 		break; */
+		/* 	} */
+		/* 	fflush(stdout); */
+		/* } */
+
+		/* /\* get hostname from the host header field - REFACTOR *\/ */
+		/* char hostname[30]; */
+		/* uint j; */
+		/* for (j = 0; j < strlen(fullHost); j++) */
+		/* { */
+		/* 	if (fullHost[j] == ':') */
+		/* 	{ */
+		/* 		hostname[j] = '\0'; */
+		/* 		break; */
+		/* 	} else { */
+		/* 		hostname[j] = fullHost[j]; */
+				
+		/* 	} */
+			
+		/* } */
+		/* fflush(stdout); */
+
+		char *entry = create_log_entry(requests[requestNum].remote_addr, cbuff, requests[requestNum].method,
+					       requests[requestNum].url, 200, st.st_size);
+		print_to_log(entry);
+
+		free(fData);
+		free(res);
+	} else {
+		/* send 404 header */
+		char *res = "HTTP/1.0 404 NOT FOUND\n"	    \
+			"Connection: close\n" \
+			"Server: mirrord/s4333060\n" \
+			"\r\n";
+
+		/* create time buffers for headers and log */
+		time_t curr;
+		struct tm *currtime;
+		time(&curr);
+		currtime = gmtime(&curr);
+		char cbuff[30];
+		strftime(cbuff, 30, "%a, %d %b %Y %T GMT", currtime);
+
+		fflush(stdout);
+
+		char *entry = create_log_entry(requests[requestNum].remote_addr, cbuff, requests[requestNum].method,
+					       requests[requestNum].url, 404, 0);
+		print_to_log(entry);
+		send(*fd, res, strlen(res), MSG_NOSIGNAL);
 	}
+
+	
 	close(*fd);
 	return 0;
 }
@@ -164,7 +255,7 @@ on_url(http_parser *parser, const char *at, size_t length)
 {
 	strncpy(requests[requestNum].url, at+1, length-1);
 	requests[requestNum].url[length] = '\0'; 
-	printf("\n\n%s\t-%d\n\n", requests[requestNum].url, requestNum);
+	/* printf("\n\n%s\t-%d\n\n", requests[requestNum].url, requestNum); */
 	fflush(stdout);
 	return 0;
 }	
@@ -172,23 +263,51 @@ on_url(http_parser *parser, const char *at, size_t length)
 int
 on_header_field(http_parser *parser, const char *at, size_t length)
 {
+	requests[requestNum].headerNum++;
 	char field[1024];
 	strncpy(field, at, length);
 	field[length] = '\0';
-	printf("FIeld: %s\n", field);
+	strncpy(requests[requestNum].headerFields[requests[requestNum].headerNum], field,
+		strlen(field));
+	return 0;
+}
+
+int
+on_header_value(http_parser *parser, const char *at, size_t length)
+{
+	int hNum =  requests[requestNum].headerNum;
+	char field[1024];
+	strncpy(field, at, length);
+	field[length] = '\0';
+	strncat(requests[requestNum].headerValues[hNum], field,
+		strlen(field));
 	return 0;
 }
 
 int
 retrieve_file(char* filepath)
 {
-	char *wd = malloc(sizeof(char) * 20);
-	char *path = getwd(wd);
-	printf("Trying to retrieve %s from %s\n", filepath, path);
-	int fd = open(filepath, O_RDONLY);
-	printf("File des %d\n", fd);
+	char *path = getcwd(NULL, 0);
 
+	/* printf("Trying to retrieve %s from %s\n", filepath, path); */
+	int fd = open(filepath, O_RDONLY);
+
+	free(path);
 	return fd;
+}
+
+char*
+create_log_entry(char *hostname, char *currentTime, char *method, char *url,
+		 int status, int nBytes)
+{
+	char *entry = malloc(sizeof(char)*CHUNK);
+	
+	/* Create the new log entry for a http request/response  */
+	ssize_t totalSize = strlen(hostname) + strlen(currentTime) +
+		strlen(method) + strlen(url) + sizeof(status) * 3+ sizeof(nBytes)* 1024;
+	snprintf(entry, totalSize, "%s [%s GMT] \"%s %s\" %d %d\n",
+		 hostname, currentTime, method, url, status, nBytes);
+	return entry;
 }
 
 int
@@ -201,9 +320,13 @@ main(int argc, char *argv[])
 
 	/* initialize requestNum global */
 	requestNum = 0;
-	
+	logFlag = 0;
+	logptr = NULL;
+
+	/* Setup http_parser settings callbacks */
 	settings.on_headers_complete = on_complete;
-	settings.on_header_value = on_header_field;
+	settings.on_header_field = on_header_field;
+	settings.on_header_value = on_header_value;
 	settings.on_url = on_url;
 
 	/* minimum number of command line arguments */
@@ -222,6 +345,7 @@ main(int argc, char *argv[])
 			break;
 		case 'd':
 			printf("dont daemonize\n");
+			logFlag = 1;
 			daemonize = 0;
 			break;
 		case 'a':
@@ -230,6 +354,8 @@ main(int argc, char *argv[])
 			logfile = fopen(log, "a+");
 			/* fputs("mirrord started.\n", logfile); */
 			fprintf(logfile, "%s", "mirrord started.\n");
+			logptr = logfile;
+			logFlag = 1;
 			fflush(logfile);
 			break;
 		case 'l':
@@ -266,7 +392,7 @@ main(int argc, char *argv[])
 
 	if (daemonize) {
 		/* daemonize the process */
-		daemon(0, 0);
+		daemon(1, 0);
 		start_mirror(logfile, address, port);
 
 	} else {
--- ass1/mirrord.h
+++ ass1/mirrord.h
@@ -4,16 +4,23 @@
 
 typedef struct request {
 	char url[MAX_SIZE];
-	int method;
+	char host[MAX_SIZE];
+	char remote_addr[16];
+	char method[MAX_SIZE];
+	char headerFields[30][30];
+	char headerValues[30][30];
+	int headerNum;
 } request;
 
 int		 on_complete(http_parser *);
 int		 on_url(http_parser *, const char *, size_t);
 int 		 start_mirror(FILE *, char *, char *);
 void 		 ack_con  (int, short, void *);
-int 		 print_to_log(FILE *, char *);
+int 		 print_to_log(char *);
 int		 on_complete(http_parser *);
 int		 retrieve_file(char*);
 int		 on_header_field(http_parser*, const char*, size_t);
+int		 on_header_value(http_parser*, const char*, size_t);
+char*		 create_log_entry(char*, char*, char*, char*, int, int);
 __dead void 	 usage(void);
 
